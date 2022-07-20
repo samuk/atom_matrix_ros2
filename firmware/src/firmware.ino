656db15c36914330badfee9de950f64b81199b0c
samuk <sam@bristolwireless.net>
2022-07-20 22:20:48 +0100

    SimpleFOC stuff

--- firmware/src/firmware.ino
+++ firmware/src/firmware.ino
@@ -32,6 +32,24 @@
 #include "SimpleFOCDrivers.h"
 #include "comms/i2c/I2CCommanderMaster.h"
 
+#define TARGET_I2C_ADDRESS 0x60
+I2CCommanderMaster commander;
+
+void setup() {
+    
+    // ...other setup code
+
+    Wire.setClock(400000);          // use same speed on target device!
+    Wire.begin();                   // initialize i2c in controller mode
+    commander.addI2CMotors(TARGET_I2C_ADDRESS, 1);            // add target device, it has 1 motor
+    //commander.addI2CMotors(TARGET_I2C_ADDRESS2, 1);         // you could add another target device on the same bus
+    //commander.addI2CMotors(TARGET_I2C_ADDRESS, 1, &wire2);  // or on a different i2c bus
+    commander.init();               // init commander
+    Wire.onReceive(onReceive);      // connect the interrupt handlers
+    Wire.onRequest(onRequest);
+    
+  // end SimpleFOC stuff  
+
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){rclErrorLoop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
